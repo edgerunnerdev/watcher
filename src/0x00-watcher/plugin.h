36dcf8e00e1354baec3cadf39f465583de721612
Pedro Nunes <pnunes515@gmail.com>
2018-10-22 21:25:39 +0100

    WIP plugins.

--- src/0x00-watcher/plugin.h
+++ src/0x00-watcher/plugin.h
@@ -2,13 +2,11 @@
 
 #ifdef _WIN32
 #ifdef _USRDLL
-#define PLUGINAPI __declspec(dllexport)
+#define PLUGIN_API __declspec(dllexport)
 #else
-#define PLUGINAPI __declspec(dllimport)
+#define PLUGIN_API __declspec(dllimport)
 #endif // _USRDLL
 #endif //_WIN32
-
-int PLUGINAPI GetPluginVersion();
 
 class Plugin
 {
@@ -16,3 +14,23 @@ public:
 	virtual bool Initialise() = 0;
 	virtual void OnMessage( const nlohmann::json& message ) = 0;
 };
+
+extern "C"
+{
+	PLUGIN_API Plugin* GetPlugin();
+}
+
+#define DECLARE_PLUGIN( PluginClass ) \
+	extern "C" \
+	{ \
+		PLUGIN_API Plugin* GetPlugin() \
+		{ \
+			static PluginClass sSingleton; \
+			return &sSingleton; \
+		} \
+		PLUGIN_API const char* GetPluginName() \
+		{ \
+			return #PluginClass; \
+		} \
+	}
+		
