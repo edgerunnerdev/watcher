f902b43305872a800bc2870089011ee288a00ac3
Pedro Nunes <pnunes515@protonmail.com>
2019-09-05 17:18:12 +0100

    WIP multipartblock.

--- src/codecmjpeg/multipartblock.cpp
+++ src/codecmjpeg/multipartblock.cpp
@@ -10,6 +10,13 @@ m_ExpectedBytes(0u)
 	//{
 	//	memcpy(&m_Bytes[0], &bytes[offset], count * sizeof(uint8_t));
 	//}
+
+	std::string header;
+	std::string headerValue;
+	while (GetHeader(bytes, offset, header, headerValue))
+	{
+
+	}
 }
 
 const std::string& MultipartBlock::GetType() const
@@ -20,4 +27,31 @@ const std::string& MultipartBlock::GetType() const
 bool MultipartBlock::IsValid() const
 {
 	return m_Bytes.size() == m_ExpectedBytes;
+}
+
+bool MultipartBlock::GetHeader(const ByteArray& bytes, size_t& offset, std::string& header, std::string& headerValue)
+{
+	size_t numBytes = bytes.size();
+
+	for (size_t i = offset; i < numBytes - 1; i++)
+	{
+		if (bytes[i] == '\r' && bytes[i+1] == '\n') // Line break
+		{
+			std::string headerData = GetString(bytes, offset, i - offset);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+std::string GetString(const ByteArray& bytes, size_t offset, size_t count)
+{
+	std::string str;
+	str.resize(count + 1);
+	for (size_t i = 0; i < count; ++i)
+	{
+		str[i] = static_cast<char>(bytes[offset + i]);
+	}
+	return str;
 }
--- src/codecmjpeg/multipartblock.h
+++ src/codecmjpeg/multipartblock.h
@@ -12,6 +12,9 @@ public:
 	bool IsValid() const;
 
 private:
+	bool GetHeader(const ByteArray& bytes, size_t& offset, std::string& header, std::string& headerValue);
+	std::string GetString(const ByteArray& bytes, size_t offset, size_t count);
+
 	std::string m_Type;
 	size_t m_ExpectedBytes;
 	ByteArray m_Bytes;
