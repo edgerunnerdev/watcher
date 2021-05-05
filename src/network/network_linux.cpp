4f9428d4134b55907a09170ee123c91dbc10da49
Pedro Nunes <pnunes515@protonmail.com>
2021-05-05 15:19:21 +0100

    Missed one from the previous commit.

--- src/network/network_linux.cpp
+++ src/network/network_linux.cpp
@@ -1,3 +1,18 @@
+// This file is part of watcher.
+//
+// watcher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// watcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with watcher. If not, see <https://www.gnu.org/licenses/>.
+
 #ifdef __linux__
 
 #include <cassert>
@@ -11,7 +26,7 @@
 
 #include "network.h"
 
-namespace Network
+namespace Watcher
 {
 
 Result ToResult( int result );
@@ -156,6 +171,6 @@ std::string ToString( Result result )
 	else return "Unknown";	
 }
 
-}
+} // namespace Watcher
 
 #endif
