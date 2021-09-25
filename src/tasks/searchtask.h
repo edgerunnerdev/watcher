d5c21f43f6348ddafd155889b158b6f9d03e35e6
Pedro Nunes <pnunes515@gmail.com>
2021-09-25 15:00:09 +0100

    WIP SearchTask

---
+++ src/tasks/searchtask.h
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////
+// This file is part of watcher.
+//
+// watcher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// watcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with watcher. If not, see <https://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <vector>
+
+#include "database/databasetime.h"
+#include "tasks/task.h"
+
+namespace Watcher
+{
+
+///////////////////////////////////////////////////////////////////////////////
+// SearchTask
+// A task intended to be used for searching for cameras, using a search
+// engine and linked to a database.
+///////////////////////////////////////////////////////////////////////////////
+
+class SearchTask : public Task
+{
+public:
+	SearchTask(const std::string& name);
+	virtual ~SearchTask() override;
+	virtual void Update(float delta) override;
+
+	virtual void Start() override;
+	virtual void Stop() override;
+
+	void AddQuery(const std::string& query);
+	void RemoveQuery(const std::string& query);
+
+	virtual void OnQuery(const std::string& query);
+	virtual void OnResult(const std::string& result, const DatabaseTime& time);
+
+private:
+	struct QueryData
+	{
+		std::string query;
+		DatabaseTime time;
+	};
+	std::vector<QueryData> m_Queries;
+};
+
+} // namespace Watcher
